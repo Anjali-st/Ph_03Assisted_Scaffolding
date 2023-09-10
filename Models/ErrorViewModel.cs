20559d19483b5e5b540b2475d05f109572e848aa
Anjali-st <anjali90908080@gmail.com>
2023-09-11 00:04:07 +0530

    Scaffolding

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sec_04Scaffolding.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
